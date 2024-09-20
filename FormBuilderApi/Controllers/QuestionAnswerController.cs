76f29a44ab46a4cebcc39545746c70369049c50b
AyushSama <aayush0029@gmail.com>
2024-09-20 18:48:56 +0530

    Added Controllers/QuestionAnswerController.cs

---
+++ FormBuilderApi/Controllers/QuestionAnswerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormBuilderApi.Controllers
+{
+    [ApiController]
+    public class QuestionAnswerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
