6d6a097e10bca07581e74fc6df7f298910850d4d
AyushSama <aayush0029@gmail.com>
2024-09-20 18:47:09 +0530

    Added GetSingle

--- FormBuilder.Application/BuisnessInterfaces/IAnswersAyushService.cs
+++ FormBuilder.Application/BuisnessInterfaces/IAnswersAyushService.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FormBuilder.Data.ModelEntities;
 
 namespace FormBuilder.Application.BuisnessInterfaces
 {
     public interface IAnswersAyushService
     {
 
-        public List<>
+        public List<QuestAnsModel> GetAll();
+
+        public List<QuestAnsModel> GetSingle();
+
 
     }
 }
--- FormBuilder.Application/BuisnessServices/AnswersAyushService.cs
+++ FormBuilder.Application/BuisnessServices/AnswersAyushService.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FormBuilder.Application.BuisnessInterfaces;
+using FormBuilder.Core;
+using FormBuilder.Data.ModelEntities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FormBuilder.Application.BuisnessServices
 {
-    public class AnswersAyushService
+    public class AnswersAyushService : IAnswersAyushService
     {
+
+        private readonly InboxContext _dbContext;
+
+        public AnswersAyushService(InboxContext inboxContext)
+        {
+            _dbContext = inboxContext;
+        }
+
+        public List<QuestAnsModel> GetAll()
+        {
+            var result = from q in _dbContext.QuestionsAyush
+                         join a in _dbContext.AnswersAyush on q.questionId equals a.questionId
+                         select new QuestAnsModel
+                         {
+                             question = q.question,
+                             answerOption = a.answerOption,
+                             answer = a.answer,
+                             correct = a.correct
+                         };
+
+            return result.ToList();
+        }
+
+        public List<QuestAnsModel> GetSingle(int questionId)
+        {
+            var result = from q in _dbContext.QuestionsAyush
+                         join a in _dbContext.AnswersAyush on q.questionId equals a.questionId
+                         where q.questionId == questionId
+                         select new QuestAnsModel
+                         {
+                             question = q.question,
+                             answerOption = a.answerOption,
+                             answer = a.answer,
+                             correct = a.correct
+                         };
+
+            return result.ToList();
+        }
+
     }
 }
